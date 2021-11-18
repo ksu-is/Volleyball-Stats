b67be0ad89dc6da5f9034dba3c9afa1676b268fe
Rclay15 <89027981+Rclay15@users.noreply.github.com>
2021-11-18 16:06:12 -0500

    Delete playerstats.py

--- playerstats.py
+++
@@ -1,8 +0,0 @@
-class Player:
-   def __init__(self, receive, serving, digs, assists, attacks, blocks):
-       self.receive = receive
-       self.serving = serving
-       self.digs = digs
-       self.assists = assists
-       self.attacks = attacks
-       self.blocks = blocks
