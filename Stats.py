cb6b4300a248c0bdd8642a0e9a021f9b832c080c
Rclay15 <89027981+Rclay15@users.noreply.github.com>
2021-11-18 16:06:17 -0500

    Create Stats.py

---
+++ Stats.py
@@ -0,0 +1,9 @@
+class Stats():
+    def __init__ (self, serve, receive, dig, attack, assist, block):
+        self.serve = serve
+        self.receive = receive
+        self.dig = dig
+        self.attack = attack
+        self.assist = assist
+        self.block = block
+        
